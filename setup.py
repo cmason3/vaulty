d8b2cddcb6c5e65f1bfc6928cebbb6b02b4bdac8
Chris Mason <57402469+cmason3@users.noreply.github.com>
2022-01-01 13:18:29 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
   ],
   packages=["vaulty"],
   include_package_data=True,
-  install_requires=["cryptography>=2.5"],
+  install_requires=["cryptography>=2.7"],
   entry_points={
     "console_scripts": [
       "vaulty=vaulty:main",
